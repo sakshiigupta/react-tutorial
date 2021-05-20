8bc5c21ceb85a5d8fc0afe580caa23f2f2c6b0de
sakshiigupta <sakshiigupta1618@gmail.com>
2021-05-20 12:13:21 +0530

    first commit

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
+import MealList from '../components/MealList';
+import { useSelector } from 'react-redux'; 
+
+const FavoritesScreen = props => {
+  
+  const faveMeals = useSelector(state => state.meals.favoriteMeals);
+
+  
+  return <MealList listData={favMeals} navigation={props.navigation} />;
+};
+
+FavoritesScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'Your Favorites',
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
+
+export default FavoritesScreen;
